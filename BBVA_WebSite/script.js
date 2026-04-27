00554e8546f40ffe110a445e1c575001f527f7ff
Juanca25-10 <juan_c.lopez_s@uao.edu.co>
2026-04-27 15:34:20 -0500

    gg
    gg

--- BBVA_WebSite/script.js
+++ BBVA_WebSite/script.js
@@ -1,13 +1,8 @@
-    // Seleccionamos el header
+  window.addEventListener('scroll', () => {
     const header = document.getElementById('bbva-header');
-
-    // Escuchamos el evento de scroll en la ventana
-    window.addEventListener('scroll', () => {
-        // Si bajamos más de 20 píxeles, le agregamos la clase que lo encoge
-        if (window.scrollY > 20) {
-            header.classList.add('header-scrolled');
-        } else {
-            // Si volvemos arriba, se la quitamos para que flote de nuevo
-            header.classList.remove('header-scrolled');
-        }
-    });
+    if (window.scrollY > 50) {
+        header.classList.add('header-scrolled');
+    } else {
+        header.classList.remove('header-scrolled');
+    }
+});
